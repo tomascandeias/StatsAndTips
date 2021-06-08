c2a95237c53553f0b4f57457108621971660c277
Tomás Candeias <tomaslcandeias@outlook.com>
2021-06-08 11:55:17 +0100

    update sql ddl + ER

--- Database/SQL_DDL.sql
+++ Database/SQL_DDL.sql
@@ -1,12 +1,12 @@
 CREATE SCHEMA SAT
 GO
 
-
 CREATE TABLE SAT.Equipa (
   id_equipa varchar(10) NOT NULL, 
   treinador varchar(10) NULL, 
   ngolos    int NULL, 
-  epoca     int NULL, 
+  epoca     int NULL,
+  Clubeid_clube varchar(10) NULL,
   PRIMARY KEY (id_equipa));
 
 CREATE TABLE SAT.Jogador (
@@ -52,9 +52,10 @@ CREATE TABLE SAT.Registo (
   PRIMARY KEY (nome));
 
 CREATE TABLE SAT.Clube (
-  id_clube      varchar(10) NULL, 
+  id_clube      varchar(10) NOT NULL, 
   nome          varchar(50) NULL, 
-  data_fundacao int NULL);
+  data_fundacao int NULL,
+  PRIMARY KEY (id_clube));
 
 CREATE TABLE SAT.Equipa_Competicao (
   Equipaid_equipa varchar(10) NOT NULL, 
@@ -67,25 +68,30 @@ CREATE TABLE SAT.SeletorOdd (
   PRIMARY KEY (id_seletor));
 
 CREATE TABLE SAT.Arbitro (
-  id_arbitro varchar(10) NULL, 
-  nome       varchar(50) NULL);
+  id_arbitro varchar(10) NOT NULL, 
+  nome       varchar(50) NULL,
+  PRIMARY KEY(id_arbitro));
 
 CREATE TABLE SAT.Administrador (
-  id_admin             varchar(10) NULL, 
+  id_admin             varchar(10) NOT NULL, 
   selecao              varchar(20) NULL, 
-  SeletorOddid_seletor varchar(10) NOT NULL);
+  SeletorOddid_seletor varchar(10) NOT NULL,
+  PRIMARY KEY(id_admin));
 
 CREATE TABLE SAT.CasaDeApostas (
-  id_casa              varchar(10) NULL, 
-  SeletorOddid_seletor varchar(10) NOT NULL);
+  id_casa              varchar(10) NOT NULL, 
+  SeletorOddid_seletor varchar(10) NOT NULL,
+  PRIMARY KEY(id_casa));
 
 CREATE TABLE SAT.Liga (
   classificacoes          varchar(255) NULL, 
-  Competicaoid_competicao int NOT NULL);
+  Competicaoid_competicao int NOT NULL,
+  PRIMARY KEY(Competicaoid_competicao));
 
-CREATE TABLE SAT.Taça (
+CREATE TABLE SAT.Taca (
   rondas                  varchar(255) NULL, 
-  Competicaoid_competicao int NOT NULL);
+  Competicaoid_competicao int NOT NULL,
+  PRIMARY KEY(Competicaoid_competicao));
 
 CREATE TABLE SAT.Jogo (
   local            int NULL, 
@@ -99,31 +105,31 @@ CREATE TABLE SAT.Jogo (
   Equipaid_equipa2 varchar(10) NOT NULL, 
   PRIMARY KEY (id_jogo));
 
-ALTER TABLE SAT.Jogador ADD CONSTRAINT Constitui FOREIGN KEY (Equipaid) REFERENCES Equipa (id_equipa);
+ALTER TABLE SAT.Jogador ADD CONSTRAINT FKJogadoreEquipaid FOREIGN KEY (Equipaid) REFERENCES SAT.Equipa (id_equipa);
 
-ALTER TABLE Equipa ADD CONSTRAINT FKEquipa401218 FOREIGN KEY (Clubeid_clube) REFERENCES Clube (id_clube);
+ALTER TABLE SAT.Equipa ADD CONSTRAINT FKEquipa401218 FOREIGN KEY (Clubeid_clube) REFERENCES SAT.Clube (id_clube);
 
-ALTER TABLE Equipa_Competicao ADD CONSTRAINT FKEquipa_Com999010 FOREIGN KEY (Equipaid_equipa) REFERENCES Equipa (id_equipa);
-ALTER TABLE Equipa_Competicao ADD CONSTRAINT FKEquipa_Com810297 FOREIGN KEY (Competicaoid) REFERENCES Competicao (id_competicao);
+ALTER TABLE SAT.Equipa_Competicao ADD CONSTRAINT FKEquipa_Com999010 FOREIGN KEY (Equipaid_equipa) REFERENCES SAT.Equipa (id_equipa);
+ALTER TABLE SAT.Equipa_Competicao ADD CONSTRAINT FKEquipa_Com810297 FOREIGN KEY (Competicaoid) REFERENCES SAT.Competicao (id_competicao);
 
-ALTER TABLE Odd ADD CONSTRAINT FKOdd956964 FOREIGN KEY (SeletorOddid_seletor) REFERENCES SeletorOdd (id_seletor);
+ALTER TABLE SAT.Odd ADD CONSTRAINT FKOdd956964 FOREIGN KEY (SeletorOddid_seletor) REFERENCES SAT.SeletorOdd (id_seletor);
 
-ALTER TABLE Administrador ADD CONSTRAINT FKAdmin919941 FOREIGN KEY (SeletorOddid_seletor) REFERENCES SeletorOdd (id_seletor);
+ALTER TABLE SAT.Administrador ADD CONSTRAINT FKAdmin919941 FOREIGN KEY (SeletorOddid_seletor) REFERENCES SAT.SeletorOdd (id_seletor);
 
-ALTER TABLE CasaDeApostas ADD CONSTRAINT FKCasaDeApos51658 FOREIGN KEY (SeletorOddid_seletor) REFERENCES SeletorOdd (id_seletor);
+ALTER TABLE SAT.CasaDeApostas ADD CONSTRAINT FKCasaDeApos51658 FOREIGN KEY (SeletorOddid_seletor) REFERENCES SAT.SeletorOdd (id_seletor);
 
-ALTER TABLE Registo ADD CONSTRAINT FKRegisto532354 FOREIGN KEY (Arbitroid_arbitro) REFERENCES Arbitro (id_arbitro);
-ALTER TABLE Registo ADD CONSTRAINT FKRegisto787259 FOREIGN KEY (Jogoid_jogo) REFERENCES Jogo (id_jogo);
+ALTER TABLE SAT.Registo ADD CONSTRAINT FKRegisto532354 FOREIGN KEY (Arbitroid_arbitro) REFERENCES SAT.Arbitro (id_arbitro);
+ALTER TABLE SAT.Registo ADD CONSTRAINT FKRegisto787259 FOREIGN KEY (Jogoid_jogo) REFERENCES SAT.Jogo (id_jogo);
 
-ALTER TABLE Jogo ADD CONSTRAINT FKJogo557265 FOREIGN KEY (Competicaoid) REFERENCES Competicao (id_competicao);
+ALTER TABLE SAT.Jogo ADD CONSTRAINT FKJogo557265 FOREIGN KEY (Competicaoid) REFERENCES SAT.Competicao (id_competicao);
 
-ALTER TABLE Odd ADD CONSTRAINT FKOdd68218 FOREIGN KEY (Jogoid_jogo) REFERENCES Jogo (id_jogo);
+ALTER TABLE SAT.Odd ADD CONSTRAINT FKOdd68218 FOREIGN KEY (Jogoid_jogo) REFERENCES SAT.Jogo (id_jogo);
 
-ALTER TABLE Liga ADD CONSTRAINT FKLiga446850 FOREIGN KEY (Competicaoid_competicao) REFERENCES Competicao (id_competicao);
+ALTER TABLE SAT.Liga ADD CONSTRAINT FKLiga446850 FOREIGN KEY (Competicaoid_competicao) REFERENCES SAT.Competicao (id_competicao);
 
-ALTER TABLE Taça ADD CONSTRAINT FKTaça681458 FOREIGN KEY (Competicaoid_competicao) REFERENCES Competicao (id_competicao);
+ALTER TABLE SAT.Taca ADD CONSTRAINT FKTaca681458 FOREIGN KEY (Competicaoid_competicao) REFERENCES SAT.Competicao (id_competicao);
 
-ALTER TABLE Jogador ADD CONSTRAINT FKJogador732794 FOREIGN KEY (Jogoid_jogo) REFERENCES Jogo (id_jogo);
+ALTER TABLE SAT.Jogador ADD CONSTRAINT FKJogador732794 FOREIGN KEY (Jogoid_jogo) REFERENCES SAT.Jogo (id_jogo);
 
-ALTER TABLE Jogo ADD CONSTRAINT FKJogo252043 FOREIGN KEY (Equipaid_equipa) REFERENCES Equipa (id_equipa);
-ALTER TABLE Jogo ADD CONSTRAINT FKJogo861332 FOREIGN KEY (Equipaid_equipa2) REFERENCES Equipa (id_equipa);
+ALTER TABLE SAT.Jogo ADD CONSTRAINT FKJogo252043 FOREIGN KEY (Equipaid_equipa) REFERENCES SAT.Equipa (id_equipa);
+ALTER TABLE SAT.Jogo ADD CONSTRAINT FKJogo861332 FOREIGN KEY (Equipaid_equipa2) REFERENCES SAT.Equipa (id_equipa);
