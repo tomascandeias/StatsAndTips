091af79d7d117f0532a20f78cc47cce74fc9d499
79afonso <apteixeira@ua.pt>
2021-06-16 15:35:52 +0100

    update

--- StatsAndTips/app/src/main/java/ihc/p7/statstips/Leagues.java
+++ StatsAndTips/app/src/main/java/ihc/p7/statstips/Leagues.java
@@ -12,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+//
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Leagues#newInstance} factory method to
